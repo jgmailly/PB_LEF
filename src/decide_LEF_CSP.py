7f7f2e843e3e3a532c21712b94e68fe44703d701
Killian-Susini <killian.susini@student-cs.fr>
2021-12-19 19:09:12 +0100

    change CSP formulation to a much better one

--- src/decide_LEF_CSP.py
+++ src/decide_LEF_CSP.py
@@ -108,19 +108,18 @@ n_constraints = 0
 problem.addConstraint(AllDifferentConstraint())
 n_constraints+=1
 
-def lamb(k,l):
+def lamb(ordObj):
     def inner(i,j):
-        return not i==k or not j==l
+        return i in ordObj[:ordObj.index(j)]
     return inner
 
 for agent_i in agents:
+    ordObj = preferences[agents.index(agent_i)]
+    ordObj = [obj for obj in ordObj if len(obj) > 0] #clean the preference (it has an empty "" usually)
     for agent_j in agents:
         if ([agent_i,agent_j] in social) or ([agent_j,agent_i] in social):
-            for obj_k in objects:
-                for obj_l in objects:
-                    if obj_k!=obj_l and not prefer(agent_i,obj_k,obj_l):
-                        problem.addConstraint(lamb(obj_k,obj_l), [agent_i, agent_j])
-                        n_constraints+=1
+            problem.addConstraint(lamb(ordObj), [agent_i, agent_j])
+            n_constraints+=1
 
 print(problem.getSolution(), n_constraints)
                             
